6017d590671d0183ed64dfe71a2cdd639ee596b3
gabriellatcc <gabriellatccorrea@gmail.com>
2025-05-27 08:03:42 -0300

    feat + fix: nova dependência maven e correção do módulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.eventually {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.mail;
+    requires java.sql;
+    requires activation;
 
     opens com.eventually.view to javafx.fxml;
     opens com.eventually to javafx.fxml;
