45afb448d117995b40e4953c15efdd1e477af7e0
gabriellatcc <gabriellatccorrea@gmail.com>
2025-06-20 09:09:51 -0300

    refactor(architecture): Otimiza UserScheduleController.java com serviços e logging - Refatora "UserScheduleController.java" delegando responsabilidades de navegação para "NavegacaoService.java" e gerenciamento de usuário/exclusão para "UsuarioSessaoService.class". - Implementa logging com SLF4J e tratamento de erros aprimorado.

--- src/main/java/com/eventually/controller/UserScheduleController.java
+++ src/main/java/com/eventually/controller/UserScheduleController.java
@@ -1,116 +1,100 @@
 package com.eventually.controller;
 
-import com.eventually.service.TelaService;
-import com.eventually.view.HomeView;
-import com.eventually.view.LoginView;
-import com.eventually.view.SettingsView;
+import com.eventually.service.AlertaService;
+import com.eventually.service.NavegacaoService;
+import com.eventually.service.UsuarioSessaoService;
 import com.eventually.view.UserScheduleView;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Classe controller da tela de programação do usuário.
- * Esta classe é responsável pela comunicação
+/** PASSÍVEL A ALTERAÇÃO
+ * Classe controladora da tela de programação do usuário, é responsável pela comunicação
  * da tela de programação com o backend.
- * @author Gabriella Tavares Costa Corrêa
+ * @author Gabriella Tavares Costa Corrêa (Construção da documentação, da classe e revisão da parte lógica da estrutura)
  * @version 1.01
- * @since 2025-04-25 (Construção da documentação da classe e revisão da estrutura)
+ * @since 2025-04-25
  */
 public class UserScheduleController {
-
     private final UserScheduleView userScheduleView;
     private final Stage primaryStage;
+
+    private UsuarioSessaoService usuarioSessaoService;
+    private NavegacaoService navegacaoService;
+
+    private String emailRecebido;
+
+    private AlertaService alertaService =new AlertaService();
+
+    private static final Logger sistemaDeLogger = LoggerFactory.getLogger(LoginController.class);
 
     /**
      * Construtor do {@code UserScheduleController}, inicializa a view de programação de usuário.
      * @param userScheduleView a interface de programação associada
      * @param primaryStage o palco principal da aplicação
      */
-    public UserScheduleController(UserScheduleView userScheduleView, Stage primaryStage) {
+    public UserScheduleController(String email, UserScheduleView userScheduleView, Stage primaryStage) {
+        this.usuarioSessaoService = UsuarioSessaoService.getInstancia();
+        sistemaDeLogger.info("Inicializado e conectado ao UsuarioSessaoService.");
+
+        this.emailRecebido = email;
+
         this.userScheduleView = userScheduleView;
-        this.primaryStage = primaryStage;
         this.userScheduleView.setUserScheduleController(this);
-        setupEventHandlersUserSchedule();
+
+        this.primaryStage = primaryStage;
+        this.navegacaoService = new NavegacaoService(primaryStage);
+
+        configManipuladoresDeEventoUS();
     }
 
-    public void setupEventHandlersUserSchedule() {
-        userScheduleView.getBtnInicio().setOnAction(e -> handleHomeButton());
-        userScheduleView.getBtnMeusEventos().setOnAction(e -> handleMyEventsButton());
-        userScheduleView.getBtnConfiguracoes().setOnAction(e -> handleSettingsButton());
-        userScheduleView.getBtnSair().setOnAction(e -> handleSairButton());
+    /**
+     * Este método configura os manipuladores de eventos para os botões da tela de programação e, em caso de falha, exibe uma
+     * mensagem no console.
+     */
+    private void configManipuladoresDeEventoUS() {
+        sistemaDeLogger.info("Método configManipuladoresEventoRegistro() chamado.");
+        try {
+            userScheduleView.getBtnProgramacao().setOnAction(e -> processarNavegacaoProgramacao());
 
+            userScheduleView.getBtnInicio().setOnAction(e -> navegacaoService.navegarParaHome(usuarioSessaoService.procurarUsuario(emailRecebido)));
+            userScheduleView.getBtnMeusEventos().setOnAction(e -> navegacaoService.navegarParaMeusEventos(emailRecebido));
+            userScheduleView.getBtnConfiguracoes().setOnAction(e -> navegacaoService.navegarParaConfiguracoes(emailRecebido));
 
-        userScheduleView.getBtnProgramacao().setOnAction(e -> handleProgramacaoButton());
-        userScheduleView.getBtnNovoEvento().setOnAction(e -> abrirNovoModal());
+            userScheduleView.getBtnSair().setOnAction(e -> navegacaoService.abrirModalEscerrrarSessão());
+            userScheduleView.getBtnNovoEvento().setOnAction(e -> navegacaoService.processarCriacaoEvento());
 
-        userScheduleView.getGrupoDatas().selectedToggleProperty().addListener((obs, oldToggle, newToggle) -> {
-            if (newToggle != null) {
-                handleDateSelection(newToggle);
-            }
-        });
+            userScheduleView.getGrupoDatas().selectedToggleProperty().addListener((obs, oldToggle, newToggle) -> {
+                if (newToggle != null) {
+                    processarSelecaoData(newToggle);
+                }});
+        } catch (Exception e) {
+            sistemaDeLogger.error("Erro ao configurar manipuladores de programação: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 
-    private void handleSairButton()
-    {
-        abrirModalEscerrrarSessão();
-        System.out.println("USController: botão de sair clicado");
-        LoginView loginView = new LoginView();
-        LoginController loginController = new LoginController(loginView, primaryStage);
-        loginView.setLoginController(loginController);
-
-        TelaService service = new TelaService();
-        Scene loginScene = new Scene(loginView,service.medirWidth(),service.medirHeight());
-
-        loginScene.getStylesheets().add(getClass().getResource("/styles/login-styles.css").toExternalForm());
-        primaryStage.setTitle("Eventually - Login");
-        primaryStage.setScene(loginScene);
+    /**
+     * Este método exibe mensagem informando que o usuário já está na tela de programação e, em caso de falha na exibição
+     * do aviso, é exibida uma mensagem de erro no console.
+     */
+    private void processarNavegacaoProgramacao() {
+        sistemaDeLogger.info("Método processarNavegacaoProgramacao() chamado.");
+        try{
+            sistemaDeLogger.info("Botão de Programação clicado!");
+            alertaService.alertarInfo("Você já está na tela de programação!");
+        } catch (Exception ex) {
+            sistemaDeLogger.error("Erro ao ir para tela de programação: "+ex.getMessage());
+            ex.printStackTrace();
+        }
     }
 
-    private void abrirModalEscerrrarSessão() {
-        System.out.println("USController: modal escerrar sessão clicado");
+    /**
+     * Seletor de datas que exibirá eventos do dia.
+     * @param toggle o clique na data por um usuário.
+     */
+    private void processarSelecaoData(javafx.scene.control.Toggle toggle) {
+        //falta: corrigir para exibir eventos (EventoUSCartao) que o usuario participa/é dele no dia clicado
+        sistemaDeLogger.info("Data selecionada: " + toggle.getUserData());
     }
-
-    private void handleDateSelection(javafx.scene.control.Toggle toggle) {System.out.println("Controller: Data selecionada: " + toggle.getUserData());}
-
-
-    private void handleHomeButton() {
-        System.out.println("Controller: Página inicial clicada");
-        HomeView homeView = new HomeView();
-        HomeController homeController = new HomeController(homeView, primaryStage);
-        homeView.setHomeController(homeController);
-
-        TelaService service = new TelaService();
-        Scene sceneHome = new Scene(homeView,service.medirWidth(),service.medirHeight());
-
-        sceneHome.getStylesheets().add(getClass().getResource("/styles/user-schedule-styles.css").toExternalForm());
-        primaryStage.setTitle("Eventually - Página inicial");
-        primaryStage.setScene(sceneHome);
-    }
-
-
-    private void handleMyEventsButton() {
-        System.out.println("Controller: Meus eventos clicado");
-    }
-
-
-    private void handleSettingsButton() {
-        System.out.println("Controller: Configurações clicado");
-
-        SettingsView settingsView = new SettingsView();
-        SettingsController settingsController = new SettingsController(settingsView, primaryStage);
-        settingsView.setSettingsController(settingsController);
-
-        TelaService service = new TelaService();
-        Scene sceneSettings = new Scene(settingsView,service.medirWidth(),service.medirHeight());
-
-        sceneSettings.getStylesheets().add(getClass().getResource("/styles/settings-styles.css").toExternalForm());
-        primaryStage.setTitle("Eventually - Configuração do Usuário");
-        primaryStage.setScene(sceneSettings);
-    }
-
-
-    private void handleProgramacaoButton() {System.out.println("Controller: Programação clicado");}
-
-    private void abrirNovoModal() {System.out.println("Controller: Novo Modal clicado");}
 }
