c74885d51ee406fb8dcd56c74c4f495a5bc985f8
gabriellatcc <gabriellatccorrea@gmail.com>
2025-06-20 08:52:40 -0300

    refactor(bootstrap): Aprimora inicialização da aplicação Main - Inicializa "UsuarioCadastroService.class" e invoca a criação de usuários padrão ao iniciar a aplicação. - Implementado logging com SLF4J e tratamento de erros no método `start()` para robustez. - Atualiza documentação Javadoc e versão da classe.

--- src/main/java/com/eventually/Main.java
+++ src/main/java/com/eventually/Main.java
@@ -1,26 +1,31 @@
 package com.eventually;
 import com.eventually.controller.LoginController;
-import com.eventually.service.TelaService;
+import com.eventually.service.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import com.eventually.view.LoginView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Classe principal da aplicação Eventually.
  * Esta classe estende {@code javafx.application.Application} e é responsável
  * por inicializar e exibir a interface gráfica do usuário.
- *
+ * @version 1.02
  * @author Yuri Garcia Maia
- * @version 1.01
  * @since 2025-04-06
- * @author Gabriella Tavares
- * @since 2025-04-22 (Documentação da classe)
+ * @author Gabriella Tavares Costa Corrêa (Documentação, revisão da estrutura e lógica da classe)
+ * @since 2025-04-22
  */
 public class Main extends Application {
     private Stage primaryStage;
-    /**
+    private UsuarioCadastroService usuarioCadastroService;
+    AlertaService alertaService = new AlertaService();
+    private static final Logger sistemaDeLogger = LoggerFactory.getLogger(Main.class);
+
+    /** PASSIVEL DE ALTERAÇÃO
      * O método principal para iniciar a aplicação JavaFX.
      * Este método é chamado pelo ambiente JavaFX quando a aplicação é lançada.
      * Ele cria a view de login, define a cena, carrega os estilos CSS,
@@ -30,27 +35,33 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
+        try {
+            this.primaryStage = primaryStage;
+            this.usuarioCadastroService = UsuarioCadastroService.getInstancia();
+            usuarioCadastroService.criarLista();
 
-        LoginView loginView = new LoginView();
-        LoginController loginController = new LoginController(loginView, primaryStage);
+            LoginView loginView = new LoginView();
+            LoginController loginController = new LoginController(loginView, primaryStage);
 
-        Scene sceneLogin = new Scene(loginView);
-        sceneLogin.getStylesheets().add(getClass().getResource("/styles/login-styles.css").toExternalForm());
+            Scene sceneLogin = new Scene(loginView);
+            sceneLogin.getStylesheets().add(getClass().getResource("/styles/login-styles.css").toExternalForm());
 
-        primaryStage.getIcons().add(new Image(getClass().getResource("/images/app-icon.png").toExternalForm()));
+            primaryStage.getIcons().add(new Image(getClass().getResource("/images/app-icon.png").toExternalForm()));
 
-        primaryStage.setTitle("Eventually - Login");
-        primaryStage.setScene(sceneLogin);
-        primaryStage.setResizable(true);
-        primaryStage.setMaximized(true);
-        primaryStage.centerOnScreen();
+            primaryStage.setTitle("Eventually - Login");
+            primaryStage.setScene(sceneLogin);
+            primaryStage.setResizable(true);
+            primaryStage.setMaximized(true);
+            primaryStage.centerOnScreen();
 
-        TelaService telaService = new TelaService();
-        telaService.aplicarTamanhoRestaurar(primaryStage, 1000, 600);
+            TelaService telaService = new TelaService();
+            telaService.aplicarTamanhoRestaurar(primaryStage, 1000, 600);
 
-        primaryStage.show();
-
+            primaryStage.show();
+        }catch (Exception e) {
+            sistemaDeLogger.error("O programa não foi iniciado deviado algum erro: "+e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     /**
