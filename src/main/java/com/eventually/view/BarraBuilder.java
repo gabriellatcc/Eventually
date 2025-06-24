965f06bdda3176e33cb8a19879db6af85a344985
gabriellatcc <gabriellatccorrea@gmail.com>
2025-06-24 07:31:18 -0300

    feat: implementa simplificação de método de criação de elemtnos vizuais e evita repetição em classes

---
+++ src/main/java/com/eventually/view/BarraBuilder.java
@@ -0,0 +1,97 @@
+package com.eventually.view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+public class BarraBuilder {
+    private Button btnInicio;
+    private Button btnMeusEventos;
+    private Button btnConfiguracoes;
+    private Button btnSair;
+    private Button btnProgramacao;
+
+    /**
+     * Este método cria a classe do container da barra superior com o nome
+     * do programa.
+     * @return a barra superior
+     */
+    public HBox construirBarraSuperior() {
+        HBox barraSuperior = new HBox();
+        barraSuperior.setPadding(new Insets(20));
+        barraSuperior.setAlignment(Pos.CENTER);
+        barraSuperior.getStyleClass().add("topbar");
+
+        Label logo = new Label("Eventually");
+        logo.getStyleClass().add("logo");
+        barraSuperior.getChildren().add(logo);
+        return barraSuperior;
+    }
+
+    /**
+     * Este método construirBarraLateral() cria uma barra lateral de navegação vertical da interface.
+     * Essa barra aparece na lateral esquerda da tela e contém botões para acessar
+     * diferentes seções do aplicativo:
+     * Página Inicial, Meus eventos, Programação, Configurações e saída.
+     * @return a barra lateral.
+     */
+    public VBox construirBarraLateral() {
+        VBox barraLateral = new VBox(20);
+        barraLateral.setPadding(new Insets(20));
+        barraLateral.getStyleClass().add("sidebar");
+        barraLateral.setPrefWidth(200);
+        barraLateral.setAlignment(Pos.TOP_CENTER);
+
+        btnInicio = new Button("Página inicial");
+        btnInicio.getStyleClass().add("menu-button");
+        btnInicio.setMaxWidth(Double.MAX_VALUE);
+        btnInicio.setPadding(new Insets(0,0,15,0));
+
+        btnMeusEventos = new Button("Meus eventos");
+        btnMeusEventos.getStyleClass().add("menu-button");
+        btnMeusEventos.setMaxWidth(Double.MAX_VALUE);
+        btnMeusEventos.setPadding(new Insets(0,0,15,0));
+
+        btnProgramacao = new Button("Programação");
+        btnProgramacao.getStyleClass().add("menu-button");
+        btnProgramacao.setMaxWidth(Double.MAX_VALUE);
+        btnProgramacao.setPadding(new Insets(0,0,15,0));
+
+        VBox parteSuperior = new VBox(15, btnInicio, btnMeusEventos, btnProgramacao);
+        parteSuperior.setPadding(new Insets(20,15,15,15));
+
+        Region espacador = new Region();
+        VBox.setVgrow(espacador, Priority.ALWAYS);
+
+        btnConfiguracoes = new Button("Configurações");
+        btnConfiguracoes.getStyleClass().add("menu-button");
+        btnConfiguracoes.setMaxWidth(Double.MAX_VALUE);
+        btnConfiguracoes.setPadding(new Insets(0,0,15,0));
+
+        btnSair = new Button("Sair");
+        btnSair.getStyleClass().add("menu-button");
+        btnSair.setMaxWidth(Double.MAX_VALUE);
+        btnSair.setPadding(new Insets(0,0,15,0));
+
+        VBox parteInferior = new VBox(15, btnConfiguracoes, btnSair);
+        parteInferior.setPadding(new Insets(0,15,40,15));
+
+        barraLateral.getChildren().addAll(parteSuperior, espacador, parteInferior);
+        barraLateral.setPadding(new Insets(0));
+        return barraLateral;
+    }
+
+    /**
+     * Métodos de encapsulamento getters
+     */
+    public Button getBtnInicio() {return btnInicio;}
+    public Button getBtnMeusEventos() {return btnMeusEventos;}
+    public Button getBtnConfiguracoes() {return btnConfiguracoes;}
+    public Button getBtnSair() {return btnSair;}
+    public Button getBtnProgramacao() {return btnProgramacao;}
+}
